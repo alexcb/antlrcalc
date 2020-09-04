7523006a809fdf5463f0bb82499d3e4ba4950bcb
Alex Couture-Beil <alex@earthly.dev>
2020-09-04 12:43:30 -0700

    working with earthfile parser

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -20,18 +20,19 @@ func (l *calcListener) ExitFromStmt(c *parser.FromStmtContext) {
 }
 
 // calc takes a string expression and returns the evaluated result.
-func calc(input string) int {
-	// Setup the input
-	is := antlr.NewInputStream(input)
-
-	// Create the Lexer
-	lexer := parser.NewEarthLexer(is)
-	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
-
-	// Create the Parser
+func calc(inputStr string) int {
+	input := antlr.NewInputStream(inputStr)
+	lexer := newLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, 0)
 	p := parser.NewEarthParser(stream)
 
-	// Finally parse the expression (by walking the tree)
+	errorListener := antlr.NewConsoleErrorListener()
+	errorStrategy := antlr.NewBailErrorStrategy()
+
+	p.AddErrorListener(errorListener)
+	p.SetErrorHandler(errorStrategy)
+	p.BuildParseTrees = true
+
 	var listener calcListener
 	antlr.ParseTreeWalkerDefault.Walk(&listener, p.EarthFile())
 
@@ -49,4 +50,58 @@ func main() {
 	fmt.Printf("got %s\n", string(s))
 
 	calc(string(s))
+}
+
+////////////////////////
+
+type lexer struct {
+	*parser.EarthLexer
+	prevIndentLevel                              int
+	indentLevel                                  int
+	afterNewLine                                 bool
+	tokenQueue                                   []antlr.Token
+	wsChannel, wsStart, wsStop, wsLine, wsColumn int
+}
+
+func newLexer(input antlr.CharStream) antlr.Lexer {
+	l := new(lexer)
+	l.EarthLexer = parser.NewEarthLexer(input)
+	return l
+}
+
+func (l *lexer) NextToken() antlr.Token {
+	peek := l.EarthLexer.NextToken()
+	ret := peek
+	switch peek.GetTokenType() {
+	case parser.EarthLexerWS:
+		if l.afterNewLine {
+			l.indentLevel++
+		}
+		l.wsChannel, l.wsStart, l.wsStop, l.wsLine, l.wsColumn =
+			peek.GetChannel(), peek.GetStart(), peek.GetStop(), peek.GetLine(), peek.GetColumn()
+	case parser.EarthLexerNL:
+		l.indentLevel = 0
+		l.afterNewLine = true
+	default:
+		if l.afterNewLine {
+			if l.prevIndentLevel < l.indentLevel {
+				l.tokenQueue = append(l.tokenQueue, l.GetTokenFactory().Create(
+					l.GetTokenSourceCharStreamPair(), parser.EarthLexerINDENT, "",
+					l.wsChannel, l.wsStart, l.wsStop, l.wsLine, l.wsColumn))
+			} else if l.prevIndentLevel > l.indentLevel {
+				l.tokenQueue = append(l.tokenQueue, l.GetTokenFactory().Create(
+					l.GetTokenSourceCharStreamPair(), parser.EarthLexerDEDENT, "",
+					l.wsChannel, l.wsStart, l.wsStop, l.wsLine, l.wsColumn))
+				l.PopMode() // Pop RECIPE mode.
+			}
+		}
+		l.prevIndentLevel = l.indentLevel
+		l.afterNewLine = false
+	}
+	if len(l.tokenQueue) > 0 {
+		l.tokenQueue = append(l.tokenQueue, peek)
+		ret = l.tokenQueue[0]
+		l.tokenQueue = l.tokenQueue[1:]
+	}
+	return ret
 }
